80303082085b2e49783a0ceab2b0c6e1b4e20f47
Parikshit <hyperloop1254@gmail.com>
2025-05-22 17:39:58 +0530

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -8,9 +8,13 @@ export default function Home () {
         <title>Home | Lightupswift</title>
         <meta name="Home" content="Home" />
       </Helmet>
+          
      
     </div>
-
+<h1>
+Music of the day
+</h1>
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6qrifdo7QINdPQr80IelGi?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     <Post/>
     </>
   )
